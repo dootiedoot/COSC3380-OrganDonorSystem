70fb204f74c46cab64b03ee4f73e31929bf1f64d
Deandre Hall <deandrehall00@gmail.com>
2017-04-04 21:32:57 -0500

    user inputs from registeration now get inserted to database

--- wwwroot/RegisterDonor.php
+++ wwwroot/RegisterDonor.php
@@ -3,7 +3,7 @@
 require "Database/MySQLconfig.php";
 
 $firstName;
-$middleInit;
+$middleInit = null;
 $lastName;
 $email;
 $sex;
@@ -19,19 +19,56 @@ if(!empty($_POST))
     {
         //  Assign variables
         $firstName = $_POST['firstName'];
-        $middleInit = $_POST['middleInit'];
+
+        //check for if middle initial has been provided
+        if(isset($_POST['middleInit'])){
+            $middleInit = $_POST['middleInit'];
+        }
+
         $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $sex = $_POST['gender'];
+        $birthDate = $_POST['birthDate'];
+        $phoneNum = $_POST['phoneNum'];
+        $weight = $_POST['weight'];
+        
+        //switch case that determines bloodtype based on 
+        switch ($_POST['bloodType']){
+            case '1':
+                $bloodType = 'A+';
+            case '2':
+                $bloodType = 'A-';
+            case '3':
+                $bloodType = 'B+';
+            case '4':
+                $bloodType = 'B-';
+            case '5':
+                $bloodType = 'AB+';
+            case '6':
+                $bloodType = 'AB-';
+            case '7':
+                $bloodType = 'O+';
+            case '8':
+                $bloodType = 'O-';
+            default:
+                $bloodType = null;
+        }
 
         // Insert data
         $sql_insert = "
-            INSERT INTO donor (FirstName, MiddleInitial, LastName) 
-            VALUES (?,?,?)
+            INSERT INTO donor (FirstName, MiddleInitial, LastName, DEmail, Sex, DateOfBirth, DPhoneNum, Weight, BloodType) 
+            VALUES (?,?,?,?,?,?,?,?)
             ";
 
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $firstName);
         $stmt->bindValue(2, $middleInit);
         $stmt->bindValue(3, $lastName);
+        $stmt->bindValue(4, $email);
+        $stmt->bindValue(5, $sex);
+        $stmt->bindValue(6, $birthDate);
+        $stmt->bindValue(7, $weight);
+        $stmt->bindValue(8, $bloodType);
         $stmt->execute();
 
         echo "<h3>Your're registered!</h3>";
@@ -45,6 +82,11 @@ if(!empty($_POST))
     unset($_POST['firstName']);
     unset($_POST['middleInit']);
     unset($_POST['lastName']);
+    unset($_POST['email']);
+    unset($_POST['sex']);
+    unset($_POST['birthDate']);
+    unset($_POST['weight']);
+    unset($_POST['bloodType']);
 
     header("Location: /");
 }
