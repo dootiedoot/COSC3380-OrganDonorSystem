7f525a15130215286e349143c48761716ee7227e
Chad Hoang <chad.hoang@yahoo.com>
2017-04-05 11:33:26 -0500

    Merged Dre's Pull Request

--- wwwroot/Action_RegisterDonor.php
+++ wwwroot/Action_RegisterDonor.php
@@ -1,6 +1,7 @@
 <?php
 
 require "Database/MySQLconfig.php";
+require "Database/updateDateAddedtoDB.php";
 
 $firstName;
 $middleInit;
@@ -21,17 +22,29 @@ if(!empty($_POST))
         $firstName = $_POST['firstName'];
         $middleInit = $_POST['middleInit'];
         $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $sex = $_POST['gender'];
+        $birthDate = $_POST['birthDate'];
+        $phoneNum = $_POST['phoneNum'];
+        $weight = $_POST['weight'];
+        $bloodType = $_POST['bloodType'];
 
         // Insert data
         $sql_insert = "
-            INSERT INTO donor (FirstName, MiddleInitial, LastName) 
-            VALUES (?,?,?)
+            INSERT INTO donor (FirstName, MiddleInitial, LastName, DEmail, Sex, DateOfBirth, DPhoneNum, Weight, BloodType) 
+            VALUES (?,?,?,?,?,?,?,?,?)
             ";
 
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $firstName);
         $stmt->bindValue(2, $middleInit);
         $stmt->bindValue(3, $lastName);
+        $stmt->bindValue(4, $email);
+        $stmt->bindValue(5, $sex);
+        $stmt->bindValue(6, $birthDate);
+        $stmt->bindValue(7, $phoneNum);
+        $stmt->bindValue(8, $weight);
+        $stmt->bindValue(9, $bloodType);
         $stmt->execute();
 
         echo "<h3>Your're registered!</h3>";
@@ -45,6 +58,11 @@ if(!empty($_POST))
     unset($_POST['firstName']);
     unset($_POST['middleInit']);
     unset($_POST['lastName']);
+    unset($_POST['email']);
+    unset($_POST['sex']);
+    unset($_POST['birthDate']);
+    unset($_POST['weight']);
+    unset($_POST['bloodType']);
 
     header("Location: /");
 }
