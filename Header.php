47320996db95479e314c0e3889ab17efeec32da8
ahussain351 <ahussain351@yahoo.com>
2017-04-12 13:05:37 -0500

    Add files via upload
    We've updated the frontpage to try and resemble an employee login portal for an organ transplant organization. the image is free use we just need to format how it will look on the page (don't know how to do that yet).

---
+++ Header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GreenHeart Transplant</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/W3.CSS Stylesheet/w3.css">
+<!--    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <style>
+        .w3-theme {color:#fff !important;background-color:#4CAF50 !important}
+        .w3-btn {background-color:#4CAF50;margin-bottom:4px}
+        .w3-code{border-left:4px solid #4CAF50}
+        .myMenu {margin-bottom:150px}
+    </style>
+</head>
+<body>
---
+++ Main_How_donation_works.php
@@ -0,0 +1,26 @@
+<div class="w3-main w3-container" style="margin-left:270px;margin-top:117px;">
+    <!--    Donor table -->
+    <div id="mainDonorsTable" class="w3-container w3-section w3-padding-large w3-card-4 w3-light-grey">
+        
+        <!--                <p>If you different hover colors, add w3-hover-<em>color</em> classes to each tr element:</p>-->
+        <!-- Information on How it works: -->
+
+	<div class="w3-panel w3-green w3-round">
+		<h1>Welcome Employees</h1>
+	</div>
+		<p>
+        <li>Click the Login button in the top right to access the employee portal</li>
+        <li>Welcome to the GreenHeart Team.</li>
+		</p>
+	<div class="w3-panel w3-green w3-round">
+		<h2>Company History</h2>
+	</div>
+		<ul class="w3-ul w3-card-4">
+			<li>Founded in 2010, GreenHeart is becoming the cornerstone institution for organ transplants around the nation. </li>
+			<li>We help connect surgeons, patients, and hospitals to aid in finding the perfect match.</li>
+		</ul>
+        echo "<img src='greenheart.jpg'>";
+
+	</div>
+    
+</div>
