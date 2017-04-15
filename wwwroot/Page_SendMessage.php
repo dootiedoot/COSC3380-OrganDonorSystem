987d50bda6c50f44311960b34abf8fd665cb9f45
Deandre Hall <deandrehall00@gmail.com>
2017-04-15 02:29:41 -0500

    added send message page

---
+++ wwwroot/Page_SendMessage.php
@@ -0,0 +1,41 @@
+
+<?php
+    session_start();
+    if( $_SESSION['userRole'] != "Admin" &&
+        $_SESSION['userRole'] != "Doctor" &&
+        $_SESSION['userRole'] != "Surgeon" &&
+        $_SESSION['userRole'] != "Donor" &&
+        $_SESSION['userRole'] != "Recipient"){
+            header("Location: /");
+        }
+?>
+
+<!--    PAGE HEADER   -->
+<?php
+    require "WebsiteContent/Header.php";
+?>
+
+<!--    Connect to the MySQL database   -->
+<?php
+    require "Database/MySQLconfig.php";
+?>
+
+<!-- Top Bar -->
+<?php
+    require "WebsiteContent/TopBar/TopBar.php";
+?>
+
+<!-- Left Sidebar -->
+<?php
+    require "WebsiteContent/LeftSidebar/LeftSidebar.php";
+?>
+
+<!-- Main content: shift it to the right by 270 pixels when the sidebar is visible -->
+<?php
+    require "WebsiteContent/MainContent/Form_SendMessage.php";
+?>
+
+<!--    PAGE FOOTER  -->
+<?php
+    require "WebsiteContent/Footer.php";
+?>
